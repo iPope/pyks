2b5de1a5c0cf9050a75ad710c7a7704d45bbdc87
Harry Smith <harry.ipope@gmail.com>
2011-08-11 13:10:09 +0100

    changed the code to be more readable

---
+++ pyks-1.0.0./pyks.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""Python Kiloseconds Manipulation Module
+Version: 1.0.0
+Created by Callum Booth (callumwbooth@gmail.com)
+Creative Commons Attribution-ShareAlike 3.0 Unported License
+Homepage: https://github.com/cbooth/pyks
+Based on the Python code from: https://github.com/bavardage/Kiloseconds
+"""
+
+import time
+
+class PyksInternalError(Exception):
+        """Base class for errors in pyks."""
+        def __init__(self, value):
+                self.parameter = value
+        def __str__(self):
+                return repr(self.parameter)
+
+def get_current_ks():
+        """Returns the current time in Kiloseconds."""
+        
+        local_time = time.localtime()
+        s_hour = float(local_time.tm_hour) * 3600
+        s_min = float(local_time.tm_min) * 60
+        sec = float(local_time.tm_sec)
+        kilo_time = (s_hour + s_min + sec) / 1000.0
+        
+        return kilo_time
+
+def ks_to_human(kilosecond):
+        """Returns the ks in a human-readable HH:MM:SS format."""
+        
+        sec = int(kilosecond * 1000)
+        human_readable = time.strftime('%H:%M:%S', time.gmtime(sec))
+        
+        return human_readable
+
+def format_ks(ks, format_ks):
+        """Returns the ks value formatted to a specific string."""
+        
+        sec = int(ks * 1000)
+
+        try:
+                formatted_string = time.strftime(format_ks, time.gmtime(sec))
+        except OverflowError as ex:
+                print ex
+        
+        return formatted_string
+        
+
+if __name__ == '__main__':
+        local_time = time.localtime()
+        print('The current time is:', time.strftime('%H:%M:%S',local_time))
+        print('In kiloseconds, this is: {0}{1}'.format(get_current_ks(), 'ks'))
+
+        format_ks(get_current_ks(), "%H:%M:%S")
+else:
+        pass
